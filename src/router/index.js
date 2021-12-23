10412990464201c6f8d5b6933067cab3ef77f4eb
kromate <akpananthony33@gmail.com>
2021-12-23 11:18:58 +0100

    feat: cloned and initialized a vite template I created before

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Landing from '../views/landing.vue'
+
 import Alert from '../views/alert.vue'
 
 const routes = [
 
   {
-    path: '/',
-    name: 'landing',
-    component: Landing,
-	meta:{
-		layout:'landing'
-	}
-  },
-  {
     path: '/alert',
     name: 'alert',
     component: Alert,
-	meta:{
-		layout:'landing'
-	}
+
   },
   {
     path: '/add',
     name: 'Add',
     component: ()=> import('../views/addAlert.vue'),
-	meta:{
-		layout:'add'
-	}
+
   }
 ]
 
